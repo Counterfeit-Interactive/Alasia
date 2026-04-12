024fa74720b4398824c641d6310e540eaf312c20
Steven Yang <95634422+G8LD@users.noreply.github.com>
2026-04-12 14:42:36 +0200

    feat: camera control

--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody3D
 
+@onready var body: Node3D = $Body
+@onready var camera: Camera3D = $CameraController/Camera3D
 
 const SPEED = 8.0
 const JUMP_VELOCITY = 6.5
@@ -10,13 +12,14 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta * FALL_VELOCITY
 
-	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	var input_dir := Input.get_vector("left", "right", "forward", "backward")
+	var input_dir := Input.get_vector("left", "right", "forward", "backward").rotated(-camera.global_rotation.y)
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		var target_angle = PI/2 - input_dir.angle()
+		body.rotation.y = rotate_toward(body.rotation.y, target_angle, 20 * delta)
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
